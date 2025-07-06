ce880b441a369887a3e3973af860d59840d60d6d
David Ho <hominhdat2009@gmail.com>
2025-07-06 18:09:51 +0700

    Add vpc resources

--- Terraform-codes/providers.tf
+++ Terraform-codes/providers.tf
@@ -8,6 +8,26 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.0"
     }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+    vpc = {
+      source  = "hashicorp/vpc"
+      version = "~> 2.0"
+    }
   }
 }
 
